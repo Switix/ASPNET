55420af33c9dcaf873ed3c5d17663aa9aa973b0d
Maciej Świtalski <maciejswitalski7@gmail.com>
2024-03-27 11:15:05 +0100

    27.03.2024

---
+++ ASPNET/Controllers/FilmyController.cs
@@ -0,0 +1,36 @@
+﻿using ASPNET.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET.Controllers
+{
+    public class FilmyController : Controller
+    {
+        FilmyContext db;
+
+        public FilmyController(FilmyContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StronyStatyczne(string nazwa)
+        {
+            return View(nazwa);
+        }
+
+        public IActionResult Lista(string nazwaKategorii)
+        {
+            var kategorie = db.Kategorie
+                .Include("Filmy")
+                .Where(kategoria=> kategoria.Nazwa.ToUpper() == nazwaKategorii.ToUpper())
+                .Single();
+            var filmy = kategorie.Filmy.ToList();
+            return View(filmy);
+        }
+    }
+}
---
+++ ASPNET/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using ASPNET.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        FilmyContext db;
+
+        public MenuViewComponent(FilmyContext db)
+        {
+            this.db = db;
+        }
+           public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("_Menu", db.Kategorie.ToList()));
+        }
+    }
+}
